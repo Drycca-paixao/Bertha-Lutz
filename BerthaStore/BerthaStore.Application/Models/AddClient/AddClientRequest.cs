f5fe8b155d263ac946c68df4acfd4b5dddb988ce
tamiresouza <tamires.crisouza@gmail.com>
2022-09-10 00:08:50 -0300

    Atualização de Models de Client

--- BerthaStore/BerthaStore.Application/Models/AddClient/AddClientRequest.cs
+++ BerthaStore/BerthaStore.Application/Models/AddClient/AddClientRequest.cs
@@ -8,5 +8,10 @@ namespace BerthaStore.Application.Models.AddClient
 {
     public class AddClientRequest
     {
+        public string Name { get; set; }
+        public string  Email { get; set; }
+        public string CPF  { get; set; }
+        public string Address { get; set; }
+        public string Password { get; set; }
     }
 }
