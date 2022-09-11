5fc24c995f99171f56313b2f9a6dc7a8b28942e0
Dindinha <indynha.w@gmail.com>
2022-09-11 10:11:48 -0300

    Update AutoMapper

--- BerthaStore/BerthaStore.Application/Mappings/MappingProfile.cs
+++ BerthaStore/BerthaStore.Application/Mappings/MappingProfile.cs
@@ -1,9 +1,8 @@
 ﻿using AutoMapper;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BerthaStore.Application.Models.NewClient;
+using BerthaStore.Application.Models.NewProduct;
+using BerthaStore.Application.Models.NewOrder;
+using BerthaStore.Core.Entities;
 
 namespace BerthaStore.Application.Mappings
 {
@@ -11,7 +10,22 @@ namespace BerthaStore.Application.Mappings
     {
         public MappingProfile()
         {
+            CreateMap<NewProductRequest, Product>()
+                .ForMember(dest => dest.Name, fonte => fonte.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Description, fonte => fonte.MapFrom(src => src.Description))
+                .ForMember(dest => dest.Price, fonte => fonte.MapFrom(src => src.Price))
+                .ForMember(dest => dest.Storage, fonte => fonte.MapFrom(src => src.Storage));
 
+            CreateMap<NewClientRequest, Client>()
+                .ForMember(dest => dest.Name, fonte => fonte.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Email, fonte => fonte.MapFrom(src => src.Email))
+                .ForMember(dest => dest.Cpf, fonte => fonte.MapFrom(src => src.Cpf))
+                .ForMember(dest => dest.Password, fonte => fonte.MapFrom(src => src.Password));
+
+            CreateMap<NewOrderRequest, Order>()
+                .ForMember(dest => dest.PaymentType, fonte => fonte.MapFrom(src => src.PaymentType));
         }
+
+
     }
 }
